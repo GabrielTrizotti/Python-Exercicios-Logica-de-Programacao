198c33ad666039c1ca0dd4c535b206ac8ec25ec7
GabrielTrizotti <113932116+GabrielTrizotti@users.noreply.github.com>
2023-02-02 17:06:52 -0300

    Create ConversorDolar.py

---
+++ Sequências Básicas/ConversorDolar.py
@@ -0,0 +1,8 @@
+# Entrada de dados (Declarando a variável "real")
+real = float( input("Insira o valor a ser convertido: R$"))
+
+# Cálculo para a conversão
+dolar = real * 5.05
+
+# Resultado da conversão
+print("Quantia em doláres: U$", dolar)
