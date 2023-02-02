e01d2d403e1861e14b053ef98ba691e6d5ffeeac
GabrielTrizotti <113932116+GabrielTrizotti@users.noreply.github.com>
2023-02-02 16:21:24 -0300

    Create AntecessorESucessor.py

---
+++ Sequências Básicas/AntecessorESucessor.py
@@ -0,0 +1,10 @@
+# Entrada de dados (Armazenando dado na variável declarada)
+num1 = ( int( input("Insira um número: ")) )
+
+# Cálculo
+antecessor = num1 - 1
+sucessor = num1 + 1
+
+# Saída de dados
+print("Sucessor do número digitado:", sucessor)
+print("Antecessor do número digitado:", antecessor)
