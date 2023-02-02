27048d213cb8a85bdd4a298bd107f47e91a0e424
GabrielTrizotti <113932116+GabrielTrizotti@users.noreply.github.com>
2023-02-02 16:24:12 -0300

    Create DobroETerceiraparte.py

---
+++ Sequências Básicas/DobroETerceiraparte.py
@@ -0,0 +1,10 @@
+# Entrada de dados (Armazenando dado na variável declarada)
+num1 = ( int( input("Insira um número: ")) )
+
+# Cálculo
+dobro = num1 + num1
+tercaParte = num1 / 3
+
+# Saída de dados
+print("Dobro do número digitado:", dobro)
+print("Terceira parte do número digitado:", tercaParte)
