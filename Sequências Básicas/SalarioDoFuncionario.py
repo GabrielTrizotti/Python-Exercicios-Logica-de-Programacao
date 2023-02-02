fb7acc610a62a5f72f2e0123ca2e8a91cfcc3e08
GabrielTrizotti <113932116+GabrielTrizotti@users.noreply.github.com>
2023-02-02 15:16:21 -0300

    Create SalarioDoFuncionario.py

---
+++ Sequências Básicas/SalarioDoFuncionario.py
@@ -0,0 +1,8 @@
+# Entrada de dados (Armazenando dado na variável declarada)
+nome = ( input("Insira seu nome: ") )
+salario = ( input("Insira o seu salário: R$") )
+
+# Saída de dados
+print("Nome do funcionário: ", nome)
+print("Salário: R$", salario)
+print("O(A) Funcionário(a)",nome,"teve um um salário de", salario,"em Junho.")
