b435e26f1486d7d29e4280799af753f28e9d4add
GabrielTrizotti <113932116+GabrielTrizotti@users.noreply.github.com>
2023-02-02 15:44:55 -0300

    Create CalculoMedia.py

---
+++ Sequências Básicas/CalculoMedia.py
@@ -0,0 +1,10 @@
+# Entrada de dados (Armazenando dado na variável declarada)
+num1 = ( float( input("Insira um número: ")) )
+num2 = ( float( input("Insira um número: ")) )
+num3 = ( float( input("Insira um número: ")) )
+
+# Cálculo da soma dos números digitados
+media = (num1 + num2 + num3) / 3
+
+# Saída de dados (Resultado da soma)
+print("O resultado da media entre as notas é:", media)
