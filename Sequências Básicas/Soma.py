4ded0bf18ed5bcfff505426e312c9785212da072
GabrielTrizotti <113932116+GabrielTrizotti@users.noreply.github.com>
2023-02-02 15:29:34 -0300

    Create Soma.py

---
+++ Sequências Básicas/Soma.py
@@ -0,0 +1,9 @@
+# Entrada de dados (Armazenando dado na variável declarada)
+num1 = ( int( input("Insira um número: ")) )
+num2 = ( int( input("Insira um número: ")) )
+
+# Cálculo da soma dos números digitados
+soma = num1 + num2 
+
+# Saída de dados (Resultado da soma)
+print("O resultado da soma entre os dois números é:", soma)
